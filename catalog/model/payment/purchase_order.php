ccb5c7022f88e42207fac73712f10ca7ae2d24f6
AwanArdy <muhammadawan46@gmail.com>
2026-09-26 04:47:25 +0000

    bug fix

--- catalog/model/payment/purchase_order.php
+++ catalog/model/payment/purchase_order.php
@@ -24,7 +24,7 @@ class PurchaseOrder extends \Opencart\System\Engine\Model {
 
 		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "zone_to_geo_zone` WHERE `geo_zone_id` = '" . (int)$this->config->get('payment_purchase_order_geo_zone_id') . "' AND `country_id` = '" . (int)($address['country_id'] ?? 0) . "' AND (`zone_id` = '" . (int)($address['zone_id'] ?? 0) . "' OR `zone_id` = '0')");
 
-		$cart_total = $this->cart->getTotal();
+		$cart_total = (float)$this->cart->getTotal();
 		$min_total = (float)$this->config->get('payment_purchase_order_total');
 
 		if ($min_total > 0 && $min_total > $cart_total) {
@@ -53,22 +53,36 @@ class PurchaseOrder extends \Opencart\System\Engine\Model {
 		$method_data = [];
 
 		if ($status) {
-			$option_data = [];
+			$title = $this->language->get('heading_title') ?: $this->language->get('text_title');
 
+			$option_data = [];
 			$option_data['purchase_order'] = [
 				'code'       => 'purchase_order.purchase_order',
-				'name'       => $this->language->get('text_title'),
+				'name'       => $title,
+				'title'      => $title,
 				'sort_order' => (int)$this->config->get('payment_purchase_order_sort_order')
 			];
 
 			$method_data = [
-				'code'       => 'purchase_order',
-				'title'      => $this->language->get('text_title'),
+				'code'       => 'purchase_order.purchase_order',
+				'name'       => $title,
+				'title'      => $title,
 				'option'     => $option_data,
 				'sort_order' => (int)$this->config->get('payment_purchase_order_sort_order')
 			];
 		}
 
 		return $method_data;
+	}
+
+	/**
+	 * Backward compatibility method for legacy / OpenCart getMethod call.
+	 *
+	 * @param array $address
+	 * @param float $total
+	 * @return array
+	 */
+	public function getMethod(array $address = [], float $total = 0): array {
+		return $this->getMethods($address);
 	}
 }
